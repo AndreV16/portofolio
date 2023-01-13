553805f581934cd876226dee7644a37d3ee57925
AndreV16 <103654005+AndreV16@users.noreply.github.com>
2023-01-13 16:32:48 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./portofolio/**/*.{html,js}"],
+  content: ["./**/*.{html,js}"],
   theme: {
     extend: {
       colors : {
